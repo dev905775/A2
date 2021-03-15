cc933eb549c3d56b8295c64a063ab061bdfbde8d
Devendra Sharma <dev905775@gmail.com>
2021-03-15 13:38:47 +0530

    first commit

--- tut1a.py
+++ tut1a.py
@@ -1,6 +1,6 @@
 import os
 def mostimpfunction():
-    print("harry is a coder")
+    print("Dev is a coders")
 if (__name__=="__main__"):   
     print(os.listdir("/"))
     print("Harry is great and he is the king of us")
